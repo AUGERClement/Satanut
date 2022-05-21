5f0bb9a660897956d9c0356e20e59f3098285318
Michou <clement.auger@epitech.eu>
2022-05-21 15:15:44 +0200

    mix new

---
+++ lib/satanut.ex
@@ -0,0 +1,18 @@
+defmodule Satanut do
+  @moduledoc """
+  Documentation for Satanut.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Satanut.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/satanut_test.exs
@@ -0,0 +1,8 @@
+defmodule SatanutTest do
+  use ExUnit.Case
+  doctest Satanut
+
+  test "greets the world" do
+    assert Satanut.hello() == :world
+  end
+end
