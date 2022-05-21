98761012e4993b36dabe376cb291fad9144f2150
Michou <clement.auger@epitech.eu>
2022-05-21 16:54:16 +0200

    Struct event

--- lib/events.ex
+++ lib/events.ex
@@ -1,3 +1,17 @@
 defmodule Satanut.Events do
   use Alchemy.Events
+  use Alchemy.Client
+
+  Events.on_message(:define_reply)
+
+  def reply("sardine") do
+    Client.send_message("")
+  end
+
+  def define_reply(message) do
+    IO.inspect(message.content)
+
+    # contain Sardine => reply("sardine")
+    # contain a communist_keyword => reply("rouge")
+  end
 end
