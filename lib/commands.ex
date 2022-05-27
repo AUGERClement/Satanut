be34a22ccb662b3593163fdfb2b344d4e530ebd9
Ghassane SEBAÏ <dexter.gogos@gmail.com>
2022-05-27 16:14:11 +0200

    Updated the sacrifice command

--- lib/commands.ex
+++ lib/commands.ex
@@ -1,5 +1,6 @@
 defmodule Satanut.Commands do
   use Alchemy.Cogs
+  alias Alchemy.Client
 
   Cogs.def sardine do
     Cogs.say("Sardine")
@@ -20,6 +21,7 @@ defmodule Satanut.Commands do
           true -> sacrifice(message, member.user.id)
           false -> Cogs.say("Puisque je suis de bonne humeur, je t'épargne.")
         end
+
       _ ->
         nil
     end
@@ -27,6 +29,29 @@ defmodule Satanut.Commands do
 
   Cogs.def sacrifice(user) do
     user_id = user |> String.replace(~r/[^\d]/, "")
-    Cogs.say("test " <> user_id <> ".")
+    stake_role_id = Application.get_env(:satanut, :stake_role_id)
+
+    Cogs.guild_id()
+    |> case do
+      {:ok, guild_id} ->
+        roles = Client.get_member(guild_id, user_id)
+          |> case do
+            {:ok, guild_member} -> guild_member.roles
+            _ -> []
+          end
+
+        IO.inspect("Guild id : " <> guild_id)
+        IO.inspect("User id : " <> user_id)
+
+        Enum.map(roles, &Client.remove_role(guild_id, user_id, &1))
+        Client.add_role(guild_id, user_id, stake_role_id)
+        Cogs.say("Et hop! Tes droits humains ont disparus!")
+        Process.sleep(40000)
+        Client.remove_role(guild_id, user_id, stake_role_id)
+        Enum.map(roles, &Client.add_role(guild_id, user_id, &1))
+        Cogs.say("Abracadabra et te revoila!")
+
+      _ -> nil
+    end
   end
 end
