cd26d190d3e2f7ba0b183da5864d6a66101cea2a
Ghassane SEBAÏ <dexter.gogos@gmail.com>
2022-05-27 15:31:05 +0200

    added redemption and sacrtifice functions

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,7 @@
 import Config
 
 config :satanut, token: System.get_env("BOT_TOKEN")
+config :satanut, stake_role_id: System.get_env("STAKE_ROLE_ID")
 
 # config :satanut, sardines_matches: ["sardine", "poisson", "mer", "vitamines", "thon", "maquereau"]
 # config :satanut, communist_matches: ["rouge", "notre", "nous", "nos", "ensemble", "communisme", "marx", "patrie"]
--- lib/commands.ex
+++ lib/commands.ex
@@ -9,8 +9,24 @@ defmodule Satanut.Commands do
     Cogs.say("Test")
   end
 
-  Cogs.def setroleid(role) do
-    Cogs.say("Le rôle bûcher a été set à : " <> role <> ".")
+  Cogs.def redemption do
+    Cogs.say("Tu oses défier le jugement du grand Satanut ?")
 
+    Cogs.member()
+    |> IO.inspect()
+    |> case do
+      {:ok, member} ->
+        case Enum.random([true, false]) do
+          true -> sacrifice(message, member.user.id)
+          false -> Cogs.say("Puisque je suis de bonne humeur, je t'épargne.")
+        end
+      _ ->
+        nil
+    end
+  end
+
+  Cogs.def sacrifice(user) do
+    user_id = user |> String.replace(~r/[^\d]/, "")
+    Cogs.say("test " <> user_id <> ".")
   end
 end
