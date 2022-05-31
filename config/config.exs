773a7ea53898e177ba66536daa0e401d96e838b9
Ghassane SEBAÏ <dexter.gogos@gmail.com>
2022-05-31 19:18:24 +0200

    Made everything generic and updated config file

--- config/config.exs
+++ config/config.exs
@@ -3,5 +3,20 @@ import Config
 config :satanut, token: System.get_env("BOT_TOKEN")
 config :satanut, stake_role_id: System.get_env("STAKE_ROLE_ID")
 
-# config :satanut, sardines_matches: ["sardine", "poisson", "mer", "vitamines", "thon", "maquereau"]
-# config :satanut, communist_matches: ["rouge", "notre", "nous", "nos", "ensemble", "communisme", "marx", "patrie"]
+config :satanut,
+  matches: [
+    {:sardine, ["sardine", "poisson", "mer", "vitamines", "thon", "maquereau"], [], [title: "Sardina pilchardus"]},
+    {:communist, ["rouge", "notre", "nous", "nos", "ensemble", "communisme", "marx", "patrie"],
+     [
+       "https://tenor.com/8pZh.gif",
+       "https://tenor.com/3JsX.gif",
+       "https://tenor.com/bG0m6.gif",
+       "https://tenor.com/bAqJF.gif",
+       "https://tenor.com/0PK5.gif",
+       "https://tenor.com/beJMZ.gif"
+     ], []},
+    {:el_psy_congroo, ["chaos", "phonewave", "micro-onde"],
+     [
+       "https://tenor.com/view/okabe-rintaro-gif-18515625"
+     ], []}
+  ]
--- lib/events.ex
+++ lib/events.ex
@@ -5,35 +5,15 @@ defmodule Satanut.Events do
 
   Events.on_message(:define_reply)
 
-  def reply(:sardine, message) do
-    sardine_embed = Embed.from_assets("sardine", title: "Sardina pilchardus")
-    Cogs.say("", embed: sardine_embed)
-  end
-
-  def reply(:communist, message) do
-    gifs = [
-      "https://tenor.com/8pZh.gif",
-      "https://tenor.com/3JsX.gif",
-      "https://tenor.com/bG0m6.gif",
-      "https://tenor.com/bAqJF.gif",
-      "https://tenor.com/0PK5.gif",
-      "https://tenor.com/beJMZ.gif"
-    ]
-
-    Cogs.say(Enum.random(gifs))
-  end
-
-  def reply(:el_psy_congroo, message) do
-    gifs = [
-      "https://tenor.com/view/okabe-rintaro-gif-18515625",
-    ]
-
-    Cogs.say(Enum.random(gifs))
-  end
-
-  def reply(keyword, message) do
-    embed = Embed.from_assets(Atom.to_string(keyword), title: "Sardina pilchardus")
-    Cogs.say("", embed: embed)
+  def reply(keyword, message, gifs, opts) do
+    case opts do
+      [] -> nil
+      options -> Cogs.say("", embed: Embed.from_assets(Atom.to_string(keyword), options))
+    end
+    case gifs do
+      [] -> nil
+      list -> Cogs.say(Enum.random(list))
+    end
   end
 
   def define_reply(message) do
@@ -41,19 +21,13 @@ defmodule Satanut.Events do
     any_in = fn (str, keywords) -> true in Enum.map(keywords, &is_in.(str, &1)) end
     downcased = String.downcase(message.content)
 
-    matches = [
-      {:sardine,  ["sardine", "poisson", "mer", "vitamines", "thon", "maquereau"]},
-      {:communist, ["rouge", "notre", "nous", "nos", "ensemble", "communisme", "marx", "patrie"]},
-      {:el_psy_congroo, ["chaos", "phonewave", "micro-onde"]}
-    ]
-
-    Enum.map(matches, fn {atom, keywords} -> if any_in.(downcased, keywords) do atom end end)
+    Enum.map(Application.get_env(:satanut, :matches), fn {_atom, keywords, _gifs, _opts}=tuple -> if any_in.(downcased, keywords) do tuple end end)
     |> IO.inspect
     |> Enum.find(&Function.identity(&1)) #First non nil val of list or nil
     |> IO.inspect
     |> case do
         nil -> nil
-        match -> reply(match, message)
+        {atom, _keywords, gifs, opts} -> reply(atom, message, gifs, opts)
       end
   end
 
